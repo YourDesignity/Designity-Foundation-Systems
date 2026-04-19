f0476c435b1837212efd50cc7d9d7cd3d74319e5
Hariprasad M.S <61413416+hariprasad-ms@users.noreply.github.com>
2026-04-19 21:29:17 +0530

    Delete backend/hr directory

--- backend/hr/__init__.py
+++
@@ -1,9 +0,0 @@
-"""HR domain services."""
-
-from backend.services.hr.attendance_service import AttendanceService
-from backend.services.hr.designation_service import DesignationService
-from backend.services.hr.employee_service import EmployeeService
-from backend.services.hr.payslip_service import PayslipService
-from backend.services.hr.schedule_service import ScheduleService
-
-__all__ = ["EmployeeService", "AttendanceService", "ScheduleService", "DesignationService", "PayslipService"]
