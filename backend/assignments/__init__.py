f3c1346302718e275d6334274e14165ee29316bd
Hariprasad M.S <61413416+hariprasad-ms@users.noreply.github.com>
2026-04-19 21:28:50 +0530

    Delete backend/assignments directory

--- backend/assignments/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Assignment domain services."""
-
-from backend.services.assignments.assignment_service import AssignmentService
-from backend.services.assignments.temporary_assignment_service import TemporaryAssignmentService
-
-__all__ = ["AssignmentService", "TemporaryAssignmentService"]
