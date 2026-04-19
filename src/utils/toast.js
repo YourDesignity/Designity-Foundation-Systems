b3aa8702e74d6c1d5cb222477001333ba65418ea
Hariprasad M.S <61413416+hariprasad-ms@users.noreply.github.com>
2026-04-19 21:46:20 +0530

    Delete src/utils directory

--- src/utils/toast.js
+++
@@ -1,9 +0,0 @@
-import { toast as reactToast } from 'react-toastify';
-
-export const toast = {
-  success: (message) => reactToast.success(message),
-  error: (message) => reactToast.error(message),
-  info: (message) => reactToast.info(message),
-  warning: (message) => reactToast.warning(message),
-  promise: (promise, messages) => reactToast.promise(promise, messages),
-};
